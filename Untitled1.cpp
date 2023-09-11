882d1450089eb13f41f2480864f695787e39a576
Tran Trong Trinh <m5c0traan@gmail.com>
2023-09-11 18:23:07 +0700

    add demo Cpp peoject

---
+++ Untitled1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+//	int a,b,tong;
+
+	printf("HelloWorld \n ");
+	
+//	getch();
+//	printf("nhap a = ");
+//	scanf("%d",&a);
+//	printf("nhap b = ");
+//	scanf("%d",&b);
+//	tong = a+b;
+//	printf("Ket qua = %d ",tong);
+}
---
+++ myfirstprogram.cpp
@@ -0,0 +1,6 @@
+#include <stuio.h>
+
+int main(){
+	printf("hello World!");
+	return 0;
+}
